629b1a5c99142b225f0ff88f2d6c9d208eead1fa
deekshith4 <deekshith@striketru.com>
2026-03-03 15:46:02 +0530

    second time

--- src/main/java/org/example/HelloLambda.java
+++ src/main/java/org/example/HelloLambda.java
@@ -10,6 +10,8 @@ public class HelloLambda implements RequestHandler<Map<String, Object>, String> 
     @Override
     public String handleRequest(Map<String, Object> input, Context context) {
         System.out.println("This is test");
+        System.out.println("This is used to to test the git reset");
+        System.out.println("This is used to to test the git reset -> second time");
         return "Hello from Lambda! Input = " + input;
     }
 }
