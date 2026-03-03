f20b8e183704eca54f5cff438254d86f4ef873cf
deekshith4 <deekshith@striketru.com>
2026-03-03 20:49:11 +0530

    Test the tetst

--- src/main/java/org/example/HelloLambda.java
+++ src/main/java/org/example/HelloLambda.java
@@ -14,6 +14,7 @@ public class HelloLambda implements RequestHandler<Map<String, Object>, String> 
         System.out.println("This is used to to test the git reset -> second time");
         System.out.println("This commit is used to checked for the login2");
         System.out.println("This commit is used to checked for feature/login3-pr");
+        System.out.println("Use the same PR ");
         return "Hello from Lambda! Input = " + input;
     }
 }
