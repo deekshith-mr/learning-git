6a92d4fa1605703c96150073d6c4999b84e7b7bb
deekshith4 <deekshith@striketru.com>
2026-03-03 19:20:56 +0530

    This is the first commit in feature/login2

--- src/main/java/org/example/HelloLambda.java
+++ src/main/java/org/example/HelloLambda.java
@@ -10,6 +10,7 @@ public class HelloLambda implements RequestHandler<Map<String, Object>, String> 
     @Override
     public String handleRequest(Map<String, Object> input, Context context) {
         System.out.println("This is test");
+        System.out.println("This commit is used to checked for the login2");
         return "Hello from Lambda! Input = " + input;
     }
 }
