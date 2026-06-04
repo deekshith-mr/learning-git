5296e1b8c0066b809b2e813553da34aed9b6aa9c
deekshith4 <deekshith@striketru.com>
2026-06-04 16:04:31 +0530

    Revert "This is for the latest changes" This reverts commit 03a4b33459d234b0984c824d73f8c468f828d7b9.

--- src/main/java/org/example/HelloLambda.java
+++ src/main/java/org/example/HelloLambda.java
@@ -29,8 +29,6 @@ public class HelloLambda implements RequestHandler<Map<String, Object>, String> 
         System.out.println("Hey this is used to test the commit");
         System.out.println("I will revert this");
         System.out.println("This is used to test the git reset soft");
-        System.out.println("This is used to test the git reset Mixed");
-
         if(4 == 4)
         {
             System.out.println("THis is just to see the error");
